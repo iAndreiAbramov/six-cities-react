f7fa6c9a446b74d89e73735aeb63d91ab59077b7
Andrei <i.andrei.abramov@gmail.com>
2022-05-17 22:23:25 +0300

    login page static content created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from 'pages/HomePage';
 import { HotelPage } from 'pages/HotelPage';
+import { LoginPage } from 'pages/LoginPage';
 
 export const App: React.FC = () => {
     return (
@@ -11,6 +12,7 @@ export const App: React.FC = () => {
             <Routes>
                 <Route path={AppRoute.Home()} element={<HomePage />} />
                 <Route path={AppRoute.Hotel('1')} element={<HotelPage />} />
+                <Route path={AppRoute.Login()} element={<LoginPage />} />
             </Routes>
         </BrowserRouter>
     );
