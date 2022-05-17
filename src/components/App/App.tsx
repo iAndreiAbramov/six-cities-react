ee141c993711113ffaa5ecf2c0dd33e987bc6892
Andrei <i.andrei.abramov@gmail.com>
2022-05-17 22:17:39 +0300

    hotel page static content created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,14 @@ import { AppRoute } from 'constants/AppRoute';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from 'pages/HomePage';
+import { HotelPage } from 'pages/HotelPage';
 
 export const App: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={AppRoute.Home()} element={<HomePage />} />
+                <Route path={AppRoute.Hotel('1')} element={<HotelPage />} />
             </Routes>
         </BrowserRouter>
     );
