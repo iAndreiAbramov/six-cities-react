1556a5c33742619a03a5f17010ef481ca9d263a8
Andrei <i.andrei.abramov@gmail.com>
2022-05-17 22:31:57 +0300

    favorites page static content created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { AppRoute } from 'constants/AppRoute';
 
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FavoritesPage } from 'pages/FavoritesPage';
 import { HomePage } from 'pages/HomePage';
 import { HotelPage } from 'pages/HotelPage';
 import { LoginPage } from 'pages/LoginPage';
@@ -11,8 +12,9 @@ export const App: React.FC = () => {
         <BrowserRouter>
             <Routes>
                 <Route path={AppRoute.Home()} element={<HomePage />} />
-                <Route path={AppRoute.Hotel('1')} element={<HotelPage />} />
+                <Route path={AppRoute.Hotel(':id')} element={<HotelPage />} />
                 <Route path={AppRoute.Login()} element={<LoginPage />} />
+                <Route path={AppRoute.Favorites()} element={<FavoritesPage />} />
             </Routes>
         </BrowserRouter>
     );
